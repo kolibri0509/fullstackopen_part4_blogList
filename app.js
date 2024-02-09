d47142f5084221583e21e8d7618475dd85eee24f
Mariya Spiridonova <kolibri0509@gmail.com>
2024-02-09 23:21:30 +0300

    step2

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const blogsRouter = require('./controllers/router')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+
+app.use(cors())
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+
+mongoose.set('strictQuery', false)
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+
+module.exports = app
