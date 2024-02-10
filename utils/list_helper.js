f8015f3bdf4c6386c751be0a420a56781763bb0a
Mariya Spiridonova <kolibri0509@gmail.com>
2024-02-10 20:06:29 +0300

    step-4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (blogs) => {
     }
     return blogs.reduce(reducer, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    let max = 0
+    blogs.forEach(blog => {
+        if(max < blog.likes){
+            max = blog.likes
+        }
+        return max
+    })
+    return blogs.length > 0
+        ? blogs.find(el => el.likes === max)
+        : 'there is no blog'
+}
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
