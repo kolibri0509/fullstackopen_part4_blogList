22c0d3f865584db12141d9b729e19a7298eb08a8
Mariya Spiridonova <kolibri0509@gmail.com>
2024-02-10 16:54:11 +0300

    step-4.4

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ mongoose.set('strictQuery', false)
 console.log('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 app.use('/api/blogs', blogsRouter)
 app.use(middleware.unknownEndpoint)
